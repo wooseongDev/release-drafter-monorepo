67a7099076e0bab286458446cb61d700e6ba7a25
wooseong <wooseong.dev@gmail.com>
2024-11-07 00:37:00 +0900

    feat: 문구 추가 (#1)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
             Get started by editing <code>app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Hello World</li>
         </ol>
 
         <div className={styles.ctas}>
